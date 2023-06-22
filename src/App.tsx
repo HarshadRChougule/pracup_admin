f4615560f0ed3141c2ce8d783281677cbe3dc058
harshad_chougule <harshadc@myelin.co.in>
2023-06-22 23:59:26 +0530

    primary commit

---
+++ src/App.tsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Main from '../src/modules/main/Main';
+import Login from '../src/modules/login/Login';
+import Register from '../src/modules/register/Register';
+import ForgetPassword from '../src/modules/forgot-password/ForgotPassword';
+import RecoverPassword from '../src/modules/recover-password/RecoverPassword';
+import { useWindowSize } from '../src/hooks/useWindowSize';
+import { calculateWindowSize } from '../src/utils/helpers';
+import { useDispatch, useSelector } from 'react-redux';
+import { setWindowSize } from '../src/store/reducers/ui';
+
+import Dashboard from '../src/pages/Dashboard';
+import Blank from '../src/pages/Blank';
+import SubMenu from '../src/pages/SubMenu';
+import Profile from '../src/pages/profile/Profile';
+
+import PublicRoute from './routes/PublicRoute';
+import PrivateRoute from './routes/PrivateRoute';
+import { setAuthentication } from './store/reducers/auth';
+import {
+  GoogleProvider,
+  getAuthStatus,
+  getFacebookLoginStatus,
+} from './utils/oidc-providers';
+
+declare const FB: any;
+
+const App = () => {
+  const windowSize = useWindowSize();
+  const screenSize = useSelector((state: any) => state.ui.screenSize);
+  const dispatch = useDispatch();
+  const [isAppLoading, setIsAppLoading] = useState(true);
+
+  const checkSession = async () => {
+    try {
+      let responses: any = await Promise.all([
+        getFacebookLoginStatus(),
+        GoogleProvider.getUser(),
+        getAuthStatus(),
+      ]);
+
+      responses = responses.filter((r: any) => Boolean(r));
+
+      if (responses && responses.length > 0) {
+        dispatch(setAuthentication(responses[0]));
+      }
+    } catch (error: any) {
+      console.log('error', error);
+    }
+    setIsAppLoading(false);
+  };
+
+  useEffect(() => {
+    checkSession();
+  }, []);
+
+  useEffect(() => {
+    const size = calculateWindowSize(windowSize.width);
+    if (screenSize !== size) {
+      dispatch(setWindowSize(size));
+    }
+  }, [windowSize]);
+
+  if (isAppLoading) {
+    return <p>Loading</p>;
+  }
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<PublicRoute />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
+        <Route path="/register" element={<PublicRoute />}>
+          <Route path="/register" element={<Register />} />
+        </Route>
+        <Route path="/forgot-password" element={<PublicRoute />}>
+          <Route path="/forgot-password" element={<ForgetPassword />} />
+        </Route>
+        <Route path="/recover-password" element={<PublicRoute />}>
+          <Route path="/recover-password" element={<RecoverPassword />} />
+        </Route>
+        <Route path="/" element={<PrivateRoute />}>
+          <Route path="/" element={<Main />}>
+            <Route path="/sub-menu-2" element={<Blank />} />
+            <Route path="/sub-menu-1" element={<SubMenu />} />
+            <Route path="/blank" element={<Blank />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/" element={<Dashboard />} />
+          </Route>
+        </Route>
+      </Routes>
+      <ToastContainer
+        autoClose={3000}
+        draggable={false}
+        position="top-right"
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnHover
+      />
+    </BrowserRouter>
+  );
+};
+
+export default App;
